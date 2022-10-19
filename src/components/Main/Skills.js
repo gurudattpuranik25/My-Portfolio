b4b56882f2e735e15b6dfd50dd529dfe30fe13ec
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2022-10-19 20:56:13 +0530

    modified style

--- src/components/Main/Skills.js
+++ src/components/Main/Skills.js
@@ -12,60 +12,65 @@ import { Link } from "react-router-dom";
 
 function Skills() {
   return (
-    <div data-aos="fade-up" data-aos-duration="2000" className="skill">
+    <div
+      // data-aos="fade-up" data-aos-duration="2000"
+      className="skill"
+    >
       <div className="skill__title">Skills.</div>
       <div className="skill__section">
         <div className="skill__grid">
           <div
-            data-aos="flip-up"
-            data-aos-duration="2000"
+            // data-aos="flip-up"
+            // data-aos-duration="2000"
             className="html skill__logo"
           >
             <img src={html} alt="" />
             <p>HTML</p>
           </div>
           <div
-            data-aos="flip-up"
-            data-aos-duration="2000"
+            // data-aos="flip-up"
+            // data-aos-duration="2000"
             className="css skill__logo"
           >
             <img src={css} alt="" />
             <p>CSS</p>
           </div>
           <div
-            data-aos="flip-up"
-            data-aos-duration="2000"
+            // data-aos="flip-up"
+            // data-aos-duration="2000"
             className="js skill__logo"
           >
             <img src={js} alt="" />
             <p>JavaScript</p>
           </div>
           <div
-            data-aos="flip-up"
-            data-aos-duration="2000"
+            // data-aos="flip-up"
+            // data-aos-duration="2000"
             className="react skill__logo"
           >
             <img src={react} alt="" />
             <p>React</p>
           </div>
           <div
-            data-aos="flip-up"
-            data-aos-duration="2000"
+            // data-aos="flip-up"
+            // data-aos-duration="2000"
             className="Tailwind skill__logo"
           >
             <img src={tailwind} alt="" />
             <p>Tailwind CSS</p>
           </div>
           <div
-            data-aos="flip-up"
-            data-aos-duration="2000"
+            // data-aos="flip-up"
+            // data-aos-duration="2000"
             className="redux skill__logo"
           >
             <img src={redux} alt="" />
             <p>Redux</p>
           </div>
         </div>
-        <div data-aos="fade-up" data-aos-duration="2000">
+        <div
+        // data-aos="fade-up" data-aos-duration="2000"
+        >
           <img className="skill__image" src={skill} alt="" />
         </div>
       </div>
