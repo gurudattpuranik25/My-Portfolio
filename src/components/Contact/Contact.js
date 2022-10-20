ee7536a7e7b8fafb96467d34718de15a236143a5
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2022-10-20 15:37:08 +0530

    added contact form functionality

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Contact.css";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_vwpuqgv",
+        "template_me1gf89",
+        form.current,
+        "QVr_TGNDSPGfMfxrI"
+      )
+      .then(
+        (result) => {
+          alert("Thank you for your messaage.");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="contact">
       <div className="contact__section">
         <div className="contact__heading">Let's chat.</div>
-        <div className="contact__form">
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <textarea cols="30" rows="9" placeholder="Message"></textarea>
-          <button className="send">Send Message</button>
-        </div>
+        <form ref={form} className="contact__form" onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" required />
+          <label>Email</label>
+          <input type="email" name="user_email" required />
+          <label>Message</label>
+          <textarea name="message" cols="30" rows="5" required />
+
+          <button type="submit" value="Send">
+            Send message
+          </button>
+        </form>
       </div>
     </div>
   );
